937251a8c797cc7b6081e4c5130fdcc56b21aaa3
Fraser Darwent <fraser@darwen.tf>
2020-08-28 13:12:05 +0100

    Neaten

--- src/index.js
+++ src/index.js
@@ -10,17 +10,28 @@ const prefixes = {
   5: "TRACE",
 };
 
+const levels = {
+  FATAL: 0,
+  ERROR: 1,
+  WARN: 2,
+  INFO: 3,
+  DEBUG: 4,
+  TRACE: 5,
+};
+
+let level = levels.INFO;
+
 function format(level, message) {
   return `${new Date().toISOString()} [${prefixes[level]}] ${message}`;
 }
 
-function log(level, message) {
+function stdOut(level, message) {
   if (filter(level)) {
     console.log(format(level, message));
   }
 }
 
-function error(level, message) {
+function stdErr(level, message) {
   if (filter(level)) {
     console.error(format(level, message));
   }
@@ -30,50 +41,50 @@ function filter(messageLevel) {
   return messageLevel <= level;
 }
 
-exports.trace = function (message) {
-  log(exports.levels.TRACE, message);
-};
+function trace(message) {
+  stdOut(levels.TRACE, message);
+}
 
-exports.debug = function (message) {
-  log(exports.levels.DEBUG, message);
-};
+function debug(message) {
+  stdOut(levels.DEBUG, message);
+}
 
-exports.info = function (message) {
-  log(exports.levels.INFO, message);
-};
+function info(message) {
+  stdOut(levels.INFO, message);
+}
+function warn(message) {
+  stdOut(levels.WARN, message);
+}
 
-exports.warn = function (message) {
-  log(exports.levels.WARN, message);
-};
+function error(message) {
+  stdErr(levels.ERROR, message);
+}
 
-exports.error = function (message) {
-  error(exports.levels.ERROR, message);
-};
-
-exports.fatal = function (message) {
-  error(exports.levels.FATAL, message);
+function fatal(message) {
+  stdErr(levels.FATAL, message);
   process.exit(1);
-};
+}
 
-exports.levels = {
-  FATAL: 0,
-  ERROR: 1,
-  WARN: 2,
-  INFO: 3,
-  DEBUG: 4,
-  TRACE: 5,
-};
-
-exports.setLevel = function (newLevel) {
-  if (newLevel < 0 || Object.keys(exports.levels).length < newLevel) {
-    error(exports.levels.ERROR, `Log level ${newLevel} is not valid`);
+function setLevel(newLevel) {
+  if (newLevel < 0 || Object.keys(levels).length < newLevel) {
+    error(`Log level ${newLevel} is not valid`);
   } else {
     level = newLevel;
   }
-};
+}
 
-exports.getLevel = function () {
+function getLevel() {
   return level;
-};
+}
 
-let level = exports.levels.INFO;
+module.exports = {
+  levels: levels,
+  setLevel: setLevel,
+  getLevel: getLevel,
+  debug: debug,
+  info: info,
+  warn: warn,
+  error: error,
+  fatal: fatal,
+  trace: trace,
+};
